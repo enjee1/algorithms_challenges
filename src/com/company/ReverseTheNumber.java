6d04476abf70ba35871d358c8711dacfd7e189a9
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-10-26 18:47:37 -0400

    Completed reverseNum()

--- src/com/company/ReverseTheNumber.java
+++ src/com/company/ReverseTheNumber.java
@@ -1,4 +1,27 @@
 package com.company;
 
+
 public class ReverseTheNumber {
+     /* This class fulfills the requirements for the challenge in the link below
+        https://edabit.com/challenge/tiyPFonvAJ8e6H9jS
+     */
+
+    public int reverseNum(int input) {
+        int reversedNum = 0;
+
+        for(;input != 0; input /= 10) {
+            int i = input % 10;
+            reversedNum = reversedNum * 10 + i;
+        }
+        
+        return reversedNum;
+    }
+
+
+    public static void main(String[] args) {
+        ReverseTheNumber revNum = new ReverseTheNumber();
+        System.out.println(revNum.reverseNum(5121));
+        System.out.println(revNum.reverseNum(69));
+        System.out.println(revNum.reverseNum(-122157));
+    }
 }
