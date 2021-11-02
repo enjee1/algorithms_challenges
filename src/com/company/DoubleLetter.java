764382c7766b07807b8e4309d0ed89f0c9711c47
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-11-01 20:48:03 -0400

    Created hasDoubleLetter(), passed Edabit tests

---
+++ src/com/company/DoubleLetter.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class DoubleLetter {
+    /* This class fulfills the requirements for the challenge in the link below
+        https://edabit.com/challenge/EaWY5d2pYBckrkAnS
+     */
+
+    public static boolean hasDoubleLetter(String word) {
+        boolean hasDouble = false;
+
+        for (int i = 0; i < word.length() - 1; i++) {
+            if (word.charAt(i) == word.charAt(i + 1)) { hasDouble = true; }
+        }
+
+        return hasDouble;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasDoubleLetter("loop"));
+        System.out.println(hasDoubleLetter("yummy"));
+        System.out.println(hasDoubleLetter("orange"));
+        System.out.println(hasDoubleLetter("munchkin"));
+    }
+}
