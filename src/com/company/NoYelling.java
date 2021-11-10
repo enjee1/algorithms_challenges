3d16cc8e24e9802b0d3c30af10d317aa76a62f65
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-11-09 20:10:37 -0500

    Initial commit

---
+++ src/com/company/NoYelling.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class NoYelling {
+    /* This class fulfills the requirements for the challenge in the link below
+        https://edabit.com/challenge/33tRK98geLPcf73PF
+     */
+
+
+    public static String noYelling(String input) {
+        String quietString = "";
+
+        return quietString;
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
