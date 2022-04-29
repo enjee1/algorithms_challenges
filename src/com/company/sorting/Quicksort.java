132f3b404327cd31701993a5de4ed51c56f6ec65
Enjee1 <22105805+enjee1@users.noreply.github.com>
2022-04-29 00:06:12 -0400

    Completed Quicksort class

---
+++ src/com/company/sorting/Quicksort.java
@@ -0,0 +1,94 @@
+package com.company.sorting;
+
+import java.util.Random;
+
+public class Quicksort {
+
+    // This code has been taken from https://youtu.be/h8eyY7dIiN4
+
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(100);
+        }
+
+        System.out.println("Before:");
+        printArray(numbers);
+
+        quicksort(numbers);
+
+        System.out.println("\nAfter:");
+        printArray(numbers);
+    }
+
+    // This overloaded method is implemented to consolidate some code in main().
+    private static void quicksort(int[] array) {
+        quicksort(array, 0, array.length - 1);
+    }
+
+    private static void quicksort(int[] array, int lowIndex, int highIndex) {
+
+        // Check if the subarray is a single element. If so, return from the method.
+        if (lowIndex >= highIndex) {
+            return;
+        }
+
+        // Select a random element within the array as a pivot and make it the last element of the array
+        int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
+        int pivot = array[pivotIndex];
+        swap(array, pivotIndex, highIndex);
+
+        int leftPointer = partition(array, lowIndex, highIndex, pivot);
+
+        quicksort(array, lowIndex, leftPointer - 1);
+        quicksort(array, leftPointer + 1, highIndex);
+
+    }
+
+    private static int partition(int[] array, int lowIndex, int highIndex, int pivot) {
+        int leftPointer = lowIndex;
+        int rightPointer = highIndex - 1;
+
+        while (leftPointer < rightPointer) {
+
+            // Move from the left until we find a number greater than the pivot, or hit the right pointer.
+            while (array[leftPointer] <= pivot && leftPointer < rightPointer) {
+                leftPointer++;
+            }
+
+            // Move from the right until we find a number less than the pivot, or hit the left pointer.
+            while (array[rightPointer] >= pivot && leftPointer < rightPointer) {
+                rightPointer--;
+            }
+
+            swap(array, leftPointer, rightPointer);
+        }
+
+        if(array[leftPointer] > array[highIndex]) {
+            swap(array, leftPointer, highIndex);
+        }
+        else {
+            leftPointer = highIndex;
+        }
+
+        return leftPointer;
+    }
+
+    private static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    private static void printArray(int[] numbers) {
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+
+
+}
+
